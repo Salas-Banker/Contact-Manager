9cde163eecc7b935c637be22589a32199f26315a
David <davidbanker22@yahoo.com>
2023-04-10 16:40:58 -0500

    initial projeect commit

---
+++ ContactManager.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
+import java.util.List;
+
+public class ContactManager {
+    public static void main(String[] args) throws IOException {
+        Path namesAndNumbers = Paths.get("contacts.txt");
+        List<String> myData = Arrays.asList("david Banker", "6897765234");
+        Files.write(namesAndNumbers, myData, StandardOpenOption.APPEND);
+        List<String> listInfo = Files.readAllLines(namesAndNumbers);
+        for (String i : listInfo) {
+            System.out.println(i);
+        }
+    }
+}
