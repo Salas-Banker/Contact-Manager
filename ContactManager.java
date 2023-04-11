14694c02ff6ca630769d96ad0826e498b05ebd72
David <davidbanker22@yahoo.com>
2023-04-11 11:56:08 -0500

    rafactored code

--- ContactManager.java
+++ ContactManager.java
@@ -9,71 +9,93 @@ import java.util.Scanner;
 
 public class ContactManager {
 
-    public static void main(String[] args) throws IOException {
-        Path namesAndNumbers = Paths.get("contacts.txt");
-        List<String> listInfo = Files.readAllLines(namesAndNumbers);
+    static Scanner userInput = new Scanner(System.in);
+    static Scanner userIntInput = new Scanner(System.in);
+    static Path namesAndNumbers = Paths.get("contacts.txt");
 
-        //add a contact
+
+
+
+    public static void main(String[] args) throws IOException {
+        contactApp();
+//        int y = startingMenu();
+
+//        switch (y) {
+//            case 1 -> viewContacts();
+//            case 2 -> addContact();
+//            case 3 -> searchContact();
+//            case 4 -> deleteContact();
+//        }
+    }
+
+    public static void contactApp() throws IOException {
+        int y = startingMenu();
+        do {
+            switch (y) {
+                case 1 -> viewContacts();
+                case 2 -> addContact();
+                case 3 -> searchContact();
+                case 4 -> deleteContact();
+                case 5 -> {
+                    break;
+                }
+            }
+        } while(true);
+    }
+
+
+    public static int startingMenu() {
         System.out.println("1. View contacts.\n" +
                 "2. Add a new contact.\n" +
                 "3. Search a contact by name.\n" +
                 "4. Delete an existing contact.\n" +
                 "5. Exit.\n" +
                 "Enter an option (1, 2, 3, 4 or 5):\n");
-
-        Scanner userChoice = new Scanner(System.in);
         System.out.println("Enter a number: ");
-        String numberPicked = userChoice.nextLine();
+        return userIntInput.nextInt();
+    }
 
-        if(numberPicked.trim().equals("1")){
-            for (String i : listInfo) {
-                System.out.printf("%n%s", i);
-            }
-        } else if (numberPicked.trim().equals("2")) {
-            //prompt the user to enter a contact
-            Scanner newContactInput = new Scanner(System.in);
-            System.out.println("Enter a name and phone number: ");
-            String userContact = newContactInput.nextLine();
-            List<String> myData = List.of(userContact);
-            Files.write(namesAndNumbers, myData, StandardOpenOption.APPEND);
-
-            //would the user like to see the users now?
-            Scanner contactsListAgain = new Scanner(System.in);
-            System.out.println("Would you like to see the contacts list? yes/no: ");
-            String usersContactsList = contactsListAgain.nextLine();
-            if(usersContactsList.toLowerCase().trim().equals("yes")){
-                for (String i : listInfo) {
-                    System.out.printf("%n%s", i);
-                }
-            }
-        } else if (numberPicked.trim().equals("3")) {
-            Scanner userSearch = new Scanner(System.in);
-            System.out.println("Enter the contacts name: ");
-            String whatUserSearched = userSearch.nextLine();
-            for (String i : listInfo) {
-                if (i.toLowerCase().contains(whatUserSearched)) {
-                    System.out.printf("%n%s", i);
-                }
-            }
-        } else if(numberPicked.trim().equals("4")) {
-            Scanner deleteContact = new Scanner(System.in);
-            System.out.println("Please give the name or number you wish to delete: ");
-            String userDeletedContact = deleteContact.nextLine();
-            System.out.println(listInfo.remove(userDeletedContact));
-            listInfo.remove(userDeletedContact);
-//            listInfo.remove(int indexOf(userDeletedContact));
-            for (String i : listInfo) {
-                System.out.printf("%n%s", i);
-            }
-            Files.write(namesAndNumbers, listInfo);
-//            Files.write(namesAndNumbers, listInfo, StandardOpenOption.APPEND);
-
-//            for(String i : listInfo) {
-//                if (userDeletedContact.toLowerCase().equals(i)) {
-//                    String x = i.replaceAll(userDeletedContact, " ");
-//                }
-//            }
+    public static void viewContacts() throws IOException {
+        Path namesAndNumbers = Paths.get("contacts.txt");
+        List<String> listInfo = Files.readAllLines(namesAndNumbers);
+        for (String i : listInfo) {
+            System.out.printf("%n%s", i);
         }
     }
+
+    public static void addContact() throws IOException {
+        //prompt the user to enter a contact
+        System.out.println("Enter a name and phone number: ");
+        String userContact = userInput.nextLine();
+        List<String> myData = List.of(userContact);
+        Files.write(namesAndNumbers, myData, StandardOpenOption.APPEND);
+    }
+
+    public static void searchContact() throws IOException {
+        List<String> listInfo = Files.readAllLines(namesAndNumbers);
+        System.out.println("Enter the contacts name: ");
+        String whatUserSearched = userInput.nextLine();
+        for (String i : listInfo) {
+            if (i.toLowerCase().contains(whatUserSearched)) {
+                System.out.printf("%n%s", i);
+            }
+        }
+    }
+
+    public static void deleteContact() throws IOException {
+        List<String> listInfo = Files.readAllLines(namesAndNumbers);
+        boolean x = false;
+        System.out.println("Please give the first and last name and number you wish to delete: ");
+        while(!x) {
+            String userDeletedContact = userInput.nextLine();
+            if (listInfo.remove(userDeletedContact)) {
+                Files.write(namesAndNumbers, listInfo);
+                x = true;
+            } else {
+                System.out.println("The contact you entered is not valid please try again:");
+            }
+        }
+    }
+
 }
 
